4a53a0c2386e77cbaae2291bd318f2a3f03e3ccb
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-01-14 11:07:16 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ import re
 app = Flask(__name__)
 
 # Base URL
-BASE_URL = 'https://www.1tamilmv.app/'
+BASE_URL = 'https://www.1tamilmv.app/index.php?/forums/forum/24-web-hd-itunes-hd-bluray/'
 
 # Function to scrape movie details
 def tamilmv():
